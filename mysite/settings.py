65e18d261c372998fca00d711a7140f38e7bc6dc
Erich Geiger <18083548+ergg@users.noreply.github.com>
2023-01-02 20:13:58 +0100

    nochmals new URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = 'django-insecure-eby*6sazyqej9$)gpz#&q-6b=i9=bm0lndh@ks0sys0le9+a!2
 DEBUG = True
 
 # For Openshift otherwise it get confused Erich 2022-02-02
-#ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['0.0.0.0'] # geht ned
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split()   # get ned
+# ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split()   # get ned
 
 # Application definition
 
